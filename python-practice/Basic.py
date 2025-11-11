6f97ce979351fdd09f30ef58972f5c7abd5ba13c
rajmohan <rajmohan.rajendran@nhsrcindiaextn.org>
2025-11-11 18:43:17 +0530

    python pract

---
+++ python-practice/Basic.py
@@ -0,0 +1,47 @@
+#1. Get input for variable name, age and address.Print it.
+""" name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+address = input("Enter your address: ")
+print(f"Name: {name}, Age: {age}, Address: {address}") """
+
+#2. Get two numbers from user and print their sum, difference, product and division.
+""" num1 = int(input("Enter Number1: "))
+num2 = int(input("Enter Number2: "))
+def add(num1, num2):
+    return num1 + num2
+
+def sub(num1, num2):
+    return num1 - num2
+
+def mul(num1, num2):
+    return num1 * num2
+
+def div(num1, num2):
+    return num1 / num2
+
+def mod(num1, num2):
+    return num1 % num2
+
+def flor(num1, num2):
+    return num1 // num2
+
+add = add(num1, num2)
+sub = sub(num1, num2)
+mul = mul(num1, num2)
+div = div(num1, num2)
+mod = mod(num1, num2)
+flor = flor(num1, num2)
+
+print(f"Addition value: {add}, Subtraction value: {sub}, Mulitiplication vlaue: {mul}, Division value: {div}, Modulus value: {mod}, floor division value: {flor}") """
+
+# Get Input for Variable name, score, department
+# Get score for 100
+# Divide 100/10
+name = input("Enter your Name: ")
+score = int(input("Enter your score: "))
+department = input("Which Department from: ")
+
+def div():
+    return score/10
+div = div()
+print(f"Name: {name}, Score: {div}/10, Department: {department}")  
