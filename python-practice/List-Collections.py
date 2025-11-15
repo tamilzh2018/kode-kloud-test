83fe4b0abd7606e7b04dac51e7c09a1f0fdedcce
rajmohan <rajmohan.rajendran@nhsrcindiaextn.org>
2025-11-15 13:23:15 +0530

    14-11

--- python-practice/List-Collections.py
+++ python-practice/List-Collections.py
@@ -8,3 +8,107 @@
 # Joining the lists: +,append(join as nested list),extend(merge-list), constructor(just provide values to initiate the object)
 # List Comprehension(purithal): Syntax: Start-Expression(what operation) for-loop End-condition
 #Nested list(Matrix):matrix1=[[0,1],[2,3]]
+"""
+append()	Adds an element at the end of the list #Syntax: list.append(elmnt)
+clear()	Removes all the elements from the list
+copy()	Returns a copy of the list
+count()	Returns the number of elements with the specified value
+extend()	Add the elements of a list (or any iterable[(list, set, tuple, etc.)]), to the end of the current list
+index()	Returns the index of the first element with the specified value. #syntax: list.index(elmnt, start, end) #Note: The index() method only returns the first occurrence of the value.
+insert()	Adds an element at the specified position #syntax: list.insert(index-position, elmnt)
+pop()	Removes the element at the specified position #Note: The pop() method returns removed value.
+remove()	Removes the item with the specified value
+reverse()	Reverses the order of the list 
+sort()	Sorts the list  # syntax: list.sort(reverse=True|False, key=myFunc).sorts ascending by default.
+
+"""
+#1.Add an element to the fruits list:
+""" fruits = ['apple', 'banana', 'cherry']
+fruits.append('kiwi')
+print(fruits)
+ """
+#2.Add a list to a list:
+""" a = ["apple", "banana", "cherry"]
+b = ["Ford", "BMW", "Volvo"]
+a.append(b)
+print(a) """
+
+#3. Copy the fruits list:
+""" fruits = ['apple', 'banana', 'cherry', 'orange']
+x = fruits.copy()
+print(x) """
+
+#4.Add the elements of cars to the fruits list:
+""" fruits = ['apple', 'banana', 'cherry']
+cars = ['Ford', 'BMW', 'Volvo']
+fruits.extend(cars)
+print(fruits) """
+
+#5.What is the position of the value "cherry":
+""" fruits = ['apple', 'banana', 'cherry']
+x = fruits.index("cherry")
+print(x) """
+
+#6.ind the postion of 'cherry', but start the search at position 4:
+""" fruits = ['apple', 'banana', 'cherry', 'kiwi', 'mango', 'orange', 'cherry']
+x = fruits.index("cherry", 4)
+print(x) """
+
+#7.Add the value "orange" as the second element of the fruit list:
+""" fruits = ['apple', 'banana', 'cherry']
+fruits.insert(1,'orange')
+print(fruits) """
+
+#8. Return the number of times the value 9 appears int the list:
+""" points = [1, 4, 2, 9, 7, 8, 9, 3, 1]
+x =points.count(9)
+print(x) """
+
+#9.Remove the second element of the fruit list:
+""" fruits = ['apple', 'banana', 'cherry', 'kiwi', 'mango', 'orange', 'cherry']
+x = fruits.pop(2)
+print(x)
+print(fruits) """
+
+#10. Remove the "banana" element of the fruit list without using index value:
+""" fruits = ['apple', 'banana', 'cherry']
+fruits.remove("banana")
+print(fruits) """
+
+#11.Reverse the order of the fruit list:
+""" fruits = ['apple', 'banana', 'cherry']
+
+fruits.reverse()
+print(fruits) """
+
+#12.Sort the list alphabetically:
+""" cars = ['Ford', 'BMW', 'Volvo']
+
+cars.sort()
+print(cars) """
+#cars.sort(reverse=True)
+
+#13.Sort the list by the length of the values:
+# A function that returns the length of the value:
+""" def myFunc(e):
+  return len(e)
+
+cars = ['Ford', 'Mitsubishi', 'BMW', 'VW']
+
+cars.sort(key=myFunc)
+print(cars) """
+
+#Sort a list of dictionaries based on the "year" value of the dictionaries:
+# A function that returns the 'year' value:
+def myFunc(e):
+  return e['year']
+
+cars = [
+  {'car': 'Ford', 'year': 2005},
+  {'car': 'Mitsubishi', 'year': 2000},
+  {'car': 'BMW', 'year': 2019},
+  {'car': 'VW', 'year': 2011}
+]
+
+cars.sort(key=myFunc)
+print(cars)
---
+++ python-practice/strings.py
@@ -0,0 +1,113 @@
+#surrounded by either single quotation marks, or double quotation marks.
+#Multiline Strings-assign a multiline string to a variable by using three quotes either double os single quotes:
+#Python does not have a character data type
+#To access elements of the string-Square brackets ex:a[index-value]
+#To get the length of a string, use the len() function.
+#Modify a string:- upper() method returns the string in upper case, lower() method returns the string in lower case,strip() method removes any whitespace from the beginning or the end, To concatenate, or combine, two strings you can use the + operator.
+#Note: All string methods return new values. They do not change the original string.
+""" 
+capitalize()	Converts the first character to upper case # syntax: string.capitalize()
+casefold()	Converts string into lower case # syntax: string.casefold()
+center()	will center align the string, using a specified character (space is default) as the fill character # syntax: string.center(length, character)
+count()	Returns the number of times a specified value occurs in a string
+encode()	Returns an encoded version of the string
+endswith()	Returns true if the string ends with the specified value
+expandtabs()	Sets the tab size of the string
+find()	Searches the string for a specified value and returns the position of where it was found #returns -1 if the value is not found.
+format()	Formats specified values in a string
+format_map()	Formats specified values in a string
+index()	Searches the string for a specified value and returns the position of where it was found #raises an exception error if the value is not found
+isalnum()	Returns True if all characters in the string are alphanumeric
+isalpha()	Returns True if all characters in the string are in the alphabet
+isascii()	Returns True if all characters in the string are ascii characters
+isdecimal()	Returns True if all characters in the string are decimals
+isdigit()	Returns True if all characters in the string are digits
+isidentifier()	Returns True if the string is an identifier
+islower()	Returns True if all characters in the string are lower case
+isnumeric()	Returns True if all characters in the string are numeric
+isprintable()	Returns True if all characters in the string are printable
+isspace()	Returns True if all characters in the string are whitespaces
+istitle()	Returns True if the string follows the rules of a title
+isupper()	Returns True if all characters in the string are upper case
+join()	Joins the elements of an iterable to the end of the string
+ljust()	Returns a left justified version of the string
+lower()	Converts a string into lower case
+lstrip()	Returns a left trim version of the string
+maketrans()	Returns a translation table to be used in translations(Ascii code)
+partition()	Returns a tuple where the string is parted into three parts
+replace()	Returns a string where a specified value is replaced with a specified value
+rfind()	Searches the string for a specified value and returns the last position of where it was found
+rindex()	Searches the string for a specified value and returns the last position of where it was found
+rjust()	Returns a right justified version of the string
+rpartition()	Returns a tuple where the string is parted into three parts
+rsplit()	Splits the string at the specified separator, and returns a list
+rstrip()	Returns a right trim version of the string
+split()	Splits the string at the specified separator, and returns a list #syntax: string.split(separator, maxsplit)
+splitlines()	Splits the string at line breaks and returns a list #syntax: string.splitlines(keeplinebreaks)
+startswith()	Returns true if the string starts with the specified value # syntax: string.startswith(value, start-position, end-position)
+strip()	Returns a trimmed version of the string #removes any leading(beginning of the string), and trailing(at the end of string) whitespaces. #syntax: string.strip(characters)
+swapcase()	Swaps cases, lower case becomes upper case and vice versa
+title()	Converts the first character of each word to upper case
+translate()	Returns a translated string(Ascii code)
+upper()	Converts a string into upper case
+zfill()	Fills the string with a specified number of 0 values at the beginning # syntax: string.zfill(len) .adds zeros (0) at the beginning of the string, until it reaches the specified length.If the value of the len parameter is less than the length of the string, no filling is done.
+
+"""
+#Note: we cannot combine strings and numbers like "My name is John, I am " + 18
+
+#1.Upper case the first letter in follwoing sentence
+""" txt = "hello, and welcome to my world."
+x = txt.capitalize()
+print(x) """
+#2.Join all items in a tuple into a string, using any character as separator:
+""" myTuple = ("John", "Peter", "Vicky")
+y = '$'.join(myTuple)
+print(y)
+ """
+#3. Remove spaces to the left of the string:
+""" txt ="  removed_left_space"
+z = txt.lstrip()
+print(z) """
+
+#4. Split a string into a list 
+""" txt = "welcome to the jungle"
+
+a = txt.split()
+print(a) """
+
+#4. Split the string, but keep the line breaks:
+""" txt = "Thank you for the music\nWelcome to the jungle"
+
+x = txt.splitlines(True)
+
+print(x) """
+
+#5.Check if the string starts with either "Hello" or "Hi":
+""" txt = "Hi, welcome to my world."
+
+x = txt.startswith(("Hello", "Hi"))
+
+print(x) """
+
+#6. Check if position 7 to 20 starts with the characters "wel":
+""" txt = "Hello, welcome to my world."
+
+x = txt.startswith("wel", 7, 20)
+
+print(x) """
+
+#6.Fill the string with zeros until it is 10 characters long:
+""" a = "hello"
+b = "welcome to the jungle"
+c = "10.000"
+
+print(a.zfill(10))
+print(b.zfill(10)) #no fill bacause string length exceeds
+print(c.zfill(10)) """
+
+#7. Replace the word S to J":
+txt = "I like bananas"
+
+x = txt.replace("s", "j")
+
+print(x)
