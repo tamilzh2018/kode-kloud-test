05d1cfd8f280d89a076d2c83a9d18b68111ae540
rajmohan <rajmohan.rajendran@nhsrcindiaextn.org>
2025-11-18 19:00:34 +0530

    desk 18-11

--- python-practice/Dictionary-Collections.py
+++ python-practice/Dictionary-Collections.py
@@ -1,11 +1,12 @@
+# We cannot set our own index value for list and tuple elements, but we can set our own index value for each element in a dictionary. That's "Key".
 # Ordered, Changable(values can change), Duplicates not Allowed because accessing bu keys if its there it will overwrite the exisitng value, Index starts from zero.
 # Dictionary are key:value pair,Accessing dict is Using keys, values, like .key(), .values(), .items(provide keys:value pair tuple), .get(key)-to access specific value without throw error
 # Add new items: use dictname[key]=value, .update(update/add specified key:value), 
 # Remove old Items: pop(removes specific key), popitem(removes last insreted pairs),del keyword,pop(), clear()
 # empty_dict_define = {}
-#count(), copy(), reverse(), sort()
-#Dictionary looping: keys(), values(), items()
-#Nested Dcit: ex:json file
+# count(), copy(), reverse(), sort()
+# Dictionary looping: keys(), values(), items()
+# Nested Dcit: ex:json file
 
 """
 clear()	Removes all the elements from the dictionary
@@ -21,3 +22,27 @@ update()	Updates the dictionary with the specified key-value pairs
 values()	Returns a list of all the values in the dictionary 
 
 """
+# List as dictionary
+""" food = ["idli", "dosai", "rice", "parota", "puri"]
+numbers = [1,2,3,4,5,3,6,7,8,9,0] #even if u have extra values then "keys" dict will not assign keys for that values simply it ommits the values
+print(dict(zip(food, numbers)))  """
+# Tuple as dictionary
+""" a = ('happy', 'learning', 'python')
+b = (1, 2, 3, 4)
+print(dict(zip(a, b))) """
+# dict function
+""" a = dict([('express', 'happy'), ('learning','book'), ('python','langauge')])  #convert list and tuple as dict
+print(a) """
+# Basic Operation in Dictionary
+# 1.Find the length
+""" a= {'express': 'happy', 'learning': 'book', 'python': 'langauge'}
+print(len(a)) """
+# 2.Concatenation (takes only two varaibles) : dict+dict not allowed
+# 3.Repetition(takes one variable and integer)dict*int not allowed
+# 4.Membership operator
+""" a= {'express': 'happy', 'learning': 'book', 'python': 'langauge'}
+print( 'express' in a) # Check with key
+print( 'express' not in a) # Check with key """
+# 5. Accessing dictionary elements
+a = {'express': 'happy', 'learning': 'book', 'python': 'langauge'}
+print(a ['express'])
--- python-practice/Tuple-Collections.py
+++ python-practice/Tuple-Collections.py
@@ -3,4 +3,46 @@
 #packing and Unpacking- syntax: start_element, *remaining_element, stop_elemnet
 # count(), index()
 #Tuples looping- in, len(), range(), enumerate()
-# empty_tuple_define = (),# Single_element_tuple_define = (value,)
+# empty_tuple_define = (),# Single_element_tuple_define = (value,) 
+# list takes more memory space but tuples takes less: check through: list_var.__sizeod__() vs tuple_var.__sizeod__()
+
+# Basic Operation in tuple
+#1. find the length
+""" num = (10,20,30,(40,50),'happy') #nested tuple content takes as single element 
+print(len(num)) """
+
+#2. tuple concatenation (takes only two varaibles)
+""" a = ('happy', 'learning', 'python')
+b = (1, 2, 3, 4)
+print(a+b) """
+
+#3. tuple repetition(takes one variable and integer)
+""" a = ('happy', 'learning', 'python')
+print(a*3) """
+
+#4. Membership operator
+""" a = ('happy', 'learning', 'python')
+print( 'python3' not in a)
+print( 'python' in a) """
+
+#5. Accessing tuple values
+""" a = ('happy', ('learning', 'python'), 20)
+print(a[1][0]) # postive indexing
+print(a[-2][-1]) # negative indexing """
+
+#6. Slicing tuple values: more than one element to fetch from tuple
+""" a = ('happy', ('learning', 'python'), 10, 20, True)
+print(a[1:3]) #postive slicing
+print(a[-1::-1]) # negative slicing """
+
+#7.  packing and un-packing in normal tuples(nested tuple will not works)
+""" variable_pack = ('happy', 'learning', 'python', 10, 20, True)
+express, book, langauge, page_start, page_end, is_it = variable_pack # assign new variables with exact values else will through error
+print(express, book, langauge, page_start, page_end, is_it) """
+
+#8. Tuple buit-in function
+""" sample_tuple = (10,2,0,31,12,54,87,10,20,10,34,2,5,2)
+print(sample_tuple.count(10))
+print(sample_tuple.index(54)) # first ocuurence values """
+
+
