caa6d58f5a70b6add8f1973bb4df6689902a25da
rajmohan <rajmohan.rajendran@nhsrcindiaextn.org>
2025-11-20 18:53:07 +0530

    set chnages

--- python-practice/Set-Collections.py
+++ python-practice/Set-Collections.py
@@ -1,5 +1,5 @@
-# collections of unique and unordered items
-#UnOrdered, Un Changable, Item Assignment not Allowed, No index logic, Duplicates Not Allowed(If ducplicates there, auto removed), 
+# collections of unique and unordered items, Entire is set mutable(we can add new values inside set) but inside set element are immutable(we can't change existing values of set becuase no indexing follows)
+#UnOrdered, Un Changable, Item Assignment not Allowed, No index logic so slicing not allowed , Duplicates Not Allowed(If ducplicates there, auto removed), 
 # Add new items: add(), update(iterables),
 # Remove old Items: remove(removes specific value incase the value dosen't exist will throw error),pop(removes randomly and also print those value),clear(),del keyword,discard(gracefully removes specific value incase the value dosen't exist will not throw error)
 #empty_set define = set()
@@ -30,3 +30,57 @@ union()	|	Return a set containing the union of sets
 update()	|=	Update the set with the union of this set and others 
 
 """
+#1. creating a set
+""" a = {1, 2, 'we', True, 3.2}
+print(a) """
+#2. creating a empty set
+""" var_a = set()
+print(var_a) """
+#3. Basic Operations like len(), min(),max(),sum() etc
+""" a = {1, 2, 'we', True, 3.2}
+print(len(a))
+b = {12, 54, 87, 3.2}
+print(min(b)) # all elements should be in integers or float
+print(max(b)) # all elements should be in integers or float
+print(sum(b)) # all elements should be in integers or float """
+
+#4. Adding an element to set - add()
+""" a = {1, 2, 'we', True, 3.2}
+a.add('new_element')
+print(a) """
+#5. Adding mulitiple element to set - update()
+""" a = {1, 2, 'we', True, 3.2}
+b = {12, 54, 87, 3.2}
+c = ['list-update', 'inside-set']
+a.update(b,c)
+print(a) """
+#6. Accessing element from set - use for loop because no indexing
+""" new_set = {1, 'we', 2, 3.2, 12, 'inside-set', 54, 87, 'list-update'}
+for each_element in new_set:
+    print(each_element) """
+    
+#7. Removing element from set - remove(),discard(),pop(), del() etc
+""" new_set = {11, 'we', 2, 3.2, 12, 'inside-set', 54, 87, 'list-update'}
+new_set.remove('we') # removes only existing value incase if you non existing value it will throws key-error
+print(new_set) 
+new_set.discard('non-exist') # if we give non-existing value it will not throws error
+print(new_set)
+new_set.pop() # removes randomly
+print(new_set)
+ """
+#8 . Built-in function follows venn diagram
+""" colors_vibgyor = {'Violet', 'Indigo', 'Blue', 'Green', 'Yellow', 'Orange', 'Red'}
+colors_rgb = {'Blue', 'Green', 'Red','White', 'Pink'}
+more_colors = {'Brown','Black','Grey','Green'}
+u = colors_vibgyor.union(colors_rgb,more_colors) # removes common values and adds non-common values, output comes with new set()
+d = colors_vibgyor.difference(colors_rgb,more_colors) # removes common values from colors_vibgyor,more_colors output comes with new set()
+i = colors_vibgyor.intersection(colors_rgb,more_colors) # keeps only common values,output comes with new set()
+sd = colors_vibgyor.symmetric_difference(colors_rgb) # removes common values from colors but keeps remaining values of both set  
+#colors_vibgyor.difference_update(colors_rgb,more_colors) # removes common values from colors_vibgyor, output overwrites existing set colors_vibgyor new-o/p:{'Orange', 'Violet', 'Yellow', 'Indigo'}
+#colors_vibgyor.intersection_update(colors_rgb) # keeps only common values,output overwrites existing set colors_vibgyor new o/p: {'Red', 'Green', 'Blue'}
+colors_vibgyor.symmetric_difference_update(colors_rgb)
+print(u)
+print(d)
+print(i)
+print(sd)
+print(colors_vibgyor) """
